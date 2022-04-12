5cb7d79dffc323a15bc6957453c5e97cc7e3dc29
Oluwadurotimi Edmond Fagbuyi <oluwadurotimi.fagbuyi@decagon.dev>
2022-04-12 15:55:09 +0100

    updated boiler plater

---
+++ handlers/handlers.go
@@ -0,0 +1,11 @@
+package handlers
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func Welcomepage(c *gin.Context) {
+	//call the HTML context to render template
+	c.HTML(http.StatusOK, "welcome-page.html", gin.H{"title": "Home page"})
+}
