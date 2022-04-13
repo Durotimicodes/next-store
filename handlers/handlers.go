93911a3c3fe11a27959d9a12cd3e707e820a318c
Oluwadurotimi Edmond Fagbuyi <oluwadurotimi.fagbuyi@decagon.dev>
2022-04-13 15:02:35 +0100

    updated handler

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -2,11 +2,8 @@ package handlers
 
 import (
 	"github.com/gin-gonic/gin"
-	"net/http"
 )
 
 func Welcomepage(c *gin.Context) {
-	//call the HTML context to render template
-	c.JSON(http.StatusOK, "welcome-page.html", gin.H{"title": "Home page"})
-}
 
+}
