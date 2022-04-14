ceaa5a001849da21541324f862995d401ac28181
Oluwadurotimi Edmond Fagbuyi <oluwadurotimi.fagbuyi@decagon.dev>
2022-04-14 10:42:52 +0100

    updated main.go file

--- main.go
+++ main.go
@@ -14,15 +14,5 @@ func main() {
 	routes.CallRoutes("port")
 
 	database.DB()
-	////Database connection
-	//db, err := gorm.Open("mysql", "root:OluwaTimi30@tcp(127.0.0.1:3306)/e-commerce_db?charset=utf8mb4&parseTime=True&loc=Local")
-	//if err != nil {
-	//	log.Fatal(err)
-	//}
-	//defer db.Close()
-
-	//DEPLOYING STATIC FILES
-	//fs := http.FileServer(http.Dir("./static"))
-	//http.Handle("/", fs)
 
 }
