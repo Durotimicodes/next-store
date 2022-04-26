74698ea0b060a2df6e1ea789f07103c5b029f5cc
Oluwadurotimi Edmond Fagbuyi <oluwadurotimi.fagbuyi@decagon.dev>
2022-04-26 03:39:26 +0100

    after SA correction

--- routes/route.go
+++ routes/route.go
@@ -74,69 +74,6 @@ func CallRoutes(port string, db *gorm.DB) {
 	// logOut LogoutUser
 	router.GET("/logout", handlers.LogoutUserHandler)
 
-	////Welcome page router
-	//router.GET("/", handlers.WelcomepageHandler)
-	//
-	////Market place router
-	//router.GET("/marketplace", handlers.MarketPlaceHandler)
-	//
-	////buyer page router
-	//router.GET("/addtocart/:id", handlers.AddToCartHandler)
-	//
-	////Buyer Page
-	//router.GET("/buyerremoveproduct/:id", handlers.RemoveProductFromCartHandler)
-	//
-	////Account Details
-	//router.GET("/buyer/accountdetails", handlers.PaymentHandler)
-	//
-	////Buyer SignUp router
-	//router.GET("/buyerssignup", handlers.BuyerSignUpPageHandler)
-	//
-	////Seller Edit Product
-	//router.GET("/sellers/editPost/:id", handlers.SellerEditProductHandler)
-	//
-	////Seller Update Product
-	//router.POST("/update-product/:id", handlers.SellerUpdateProductHandler)
-	//
-	////Admin Post Product
-	//router.POST("/sellers/addproducts", handlers.AdminPostProductHandler)
-	//
-	////Buyer Page
-	//router.GET("/buyerpage", handlers.BuyerPageHandler)
-	//
-	////Admin Get Product
-	//router.GET("/sellers/addproductspage", handlers.AdminGetProductHandler)
-	//
-	////Admin Delete Product
-	//router.GET("/sellers/deleteproduct/:id", handlers.AdminDeleteProductHandler)
-	//
-	////Admin Launch Product to Market Place
-	//router.POST("/seller/postproduct", handlers.AdminPostInMarketHandler)
-	//router.GET("/sellers/launchproduct", handlers.AdminLaunchProductHandler)
-	//
-	////SIGN UP AND LOGIN
-	//router.GET("/sellersignup", handlers.SellerLoginPageHandler)
-	//
-	//// logOut LogoutUser
-	//router.GET("/logout", handlers.LogoutUserHandler)
-	//
-	////Seller page router
-	//router.GET("/sellerpage", handlers.SellerPageHandler)
-	//
-	////Seller Login
-	//router.POST("/sellers/signin", handlers.SellerLoginHandler)
-	//
-	////Buyer Sig-up
-	//router.POST("/buyers/signup", handlers.BuyerSignUpHandler)
-	////Buyer Login-in
-	//router.POST("/buyers/login", handlers.LoginHandler)
-	//
-	////To search for product
-	//router.GET("/searchproduct", handlers.SearchProduct)
-	//
-	////Adminstrator's Dashboard
-	//router.GET("/admindashboard", handlers.AdminDashBoard)
-
 	//start and run the server on port 8084
 	port = ":" + os.Getenv("PORT")
 	if port == ":" {
